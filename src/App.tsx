0d0d1733dc78b5a0e6c93cee5f1e0b87769ac6d7
Horziox <horziox.dev@gmail.com>
2023-01-26 11:15:51 +0100

    example with ressources

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import { Client } from 'rr-apilib';
-
-const client = new Client();
+import React, { useEffect, useState } from 'react';
+import { Client, Resource } from 'rr-apilib';
 
 export default function App() {
 
-    console.log(client)
+    const [client, setClient] = useState(new Client());
+    const [ressources, setRessources] = useState<Resource[]>([]);
+
+    async function getRessources() {
+        return (await client.resources.fetchAll()).map(r => r);
+    }
+
+    useEffect(() => {
+        // console.log("client change")
+
+        getRessources()
+            .then(res => setRessources(res))
+
+    }, [client])
     
     return (
         <div className="App">
-            {client.resources.cache.map((r, i) => (
-                <div key={i}>{r.title}</div>
-            ))}
+            {ressources.map((r, i) => {
+                return (
+                    <div key={i}>
+                        <h5>{r.title}</h5>
+                        {r.description && (
+                            <p>{r.description}</p>
+                        )}
+                    </div>
+                )
+            }
+            )}
         </div>
     );
 }
