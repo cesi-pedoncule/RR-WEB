14b1b9f7172675c95ac9881546c51a962210481c
HM22-code <H.M-22@outlook.fr>
2023-04-06 13:07:44 +0200

    Add CategoryCard component and style

---
+++ src/components/Card/CategoryCard.tsx
@@ -0,0 +1,30 @@
+import { Category, Resource } from 'rr-apilib';
+
+import CategoryCardStyles from "../../styles/Components/Card/CategoryCardStyles.module.css";
+import { useNavigate } from 'react-router-dom';
+
+interface Props {
+    category: Category;
+    resources:  Resource[]
+}
+
+export default function CategoryCard({ category, resources }:Props) {
+    
+    const navigate = useNavigate();
+
+    const handleOnPress = () => {
+
+        if (category.resources.cache.size === 0) {
+            alert("Cette catégorie ne contient aucune ressource.")
+        } else {
+            navigate('category-detail')
+        }
+    }
+    
+    return (
+        <div className={CategoryCardStyles.cardBackground} onClick={handleOnPress}>
+            <h2 className={CategoryCardStyles.cardTitle}>{category.name}</h2>
+            <p className={CategoryCardStyles.cardText}>{category.resources.cache.size.toString() + " Ressource(s)"}</p>
+        </div>
+    )
+}
