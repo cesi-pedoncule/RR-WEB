c75787df4abfd881a626b44afa71dd333d8a0b34
HM22-code <H.M-22@outlook.fr>
2023-04-22 19:18:34 +0200

    Add IconButtons on ResourceCard

--- src/components/Card/ResourceCard.tsx
+++ src/components/Card/ResourceCard.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Resource } from "rr-apilib";
 import ResourceCardStyles from "../../styles/Components/Card/ResourceCardStyles.module.css";
 import CategoryButton from "../Button/CategoryButton";
+import CommentButton from "../Button/CommentButton";
+import LikeButton from "../Button/LikeButton";
 
 interface Props {
     resourceData: Resource;
@@ -24,6 +26,10 @@ export default function ResourceCard ({ resourceData, styleContainer}: Props) {
         <div className={ResourceCardStyles.container} onClick={onClickDetailResource}>
             <div className={ResourceCardStyles.lineButtonsAndUser}>
                 <p className={ResourceCardStyles.cardUser}>{username}</p>
+                <div className={ResourceCardStyles.userAndButtonsContainer}>
+                    <LikeButton resource={resource}/>
+                    <CommentButton commentNumber={numberCommentResource}/>
+                </div>
             </div>
             <p className={ResourceCardStyles.cardTitle}>{resource.title}</p>
             <div className={ResourceCardStyles.categoriesContainer}>
