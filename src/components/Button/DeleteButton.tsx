7f93a3b313ff2a106a964cdf7d644c0e5670ea6a
HM22-code <H.M-22@outlook.fr>
2023-04-22 21:24:13 +0200

    Add DeleteButton

---
+++ src/components/Button/DeleteButton.tsx
@@ -0,0 +1,14 @@
+import DeleteButtonStyles from "../../styles/Components/Button/DeleteButtonStyles.module.css";
+import { BsTrash } from "react-icons/bs"
+
+interface Props {
+	callBack: () => void;
+}
+
+export default function DeleteButton({ callBack }: Props) {
+    return (
+        <div className={DeleteButtonStyles.buttonContainer} onClick={callBack}>
+            <BsTrash />
+        </div>
+    )
+}
