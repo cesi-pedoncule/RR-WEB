8e0f39cb77dc06725cb65b37e4cc1898cce70dbe
Horziox <horziox.dev@gmail.com>
2023-04-22 20:27:20 +0200

    categories page search

--- src/pages/CategoriesPage.tsx
+++ src/pages/CategoriesPage.tsx
@@ -1,5 +1,6 @@
-import { useCallback, useEffect, useState } from "react";
-import { Category, Client } from "rr-apilib";
+import { useState } from "react";
+import { Client } from "rr-apilib";
+
 import CommonStyles from "../styles/CommonStyles.module.css";
 import CategoryCard from "../components/Card/CategoryCard";
 import SearchBar from "../components/Input/SearchBar";
@@ -10,38 +11,28 @@ interface Props {
 
 export default function CategoriesPage ({ client }: Props) {
 
-    const [categories, setCategories] = useState<Category[]>([]);
-    const [ categoriesFiltered, setCategoriesFiltered ] = useState<Category[]>([]);
+    const [ search, setSearch ] = useState('');
 
     const handleChangeSearch = (text: string) => {
-        const filteredCategories = Array.from(client.categories.cache.values()).filter((category) => 
-            category.name.toLowerCase().includes(text.toLowerCase())
-        );
-        setCategories([...filteredCategories]);
-        setCategoriesFiltered([...filteredCategories.slice(0, 8)]);
+        setSearch(text);
     }
-
-    const onRefresh = useCallback(async () => {
-        const refreshCategories = Array.from(client.categories.cache.values());
-		setCategories([ ...refreshCategories ]);
-		setCategoriesFiltered([ ...refreshCategories.slice(0, 8) ]);
- 	 }, []);
-
-    useEffect(() => {
-        onRefresh();
-    }, [client])
 
     return (
         <div className={CommonStyles.container}>
             <div className={CommonStyles.content}>
+                
                 <h1 className={CommonStyles.title}>Les catégories</h1>
                 
-                <SearchBar onChangeSearch={handleChangeSearch} />
+                <SearchBar value={search} onChangeSearch={handleChangeSearch} />
                 
                 <div className={CommonStyles.itemsContainer}>
-                    {categoriesFiltered.map((q, i) => 
-                        <CategoryCard key={i} category={q}/>
-                    )}
+                    {client.categories.cache.map((category, id) => {
+                        if(category.name.toLowerCase().includes(search)) {
+                            return (
+                                <CategoryCard key={id} category={category} />
+                            )
+                        }
+                    })}
                 </div>
             </div>
         </div>
