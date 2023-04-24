e8a72fd17422042c1fd68d32416d9193a4bae4c0
HM22-code <H.M-22@outlook.fr>
2023-04-24 21:45:08 +0200

    Fix admin menu buttons size

--- src/pages/Admin/AdminMenuPage.tsx
+++ src/pages/Admin/AdminMenuPage.tsx
@@ -21,23 +21,23 @@ export default function AdminUsersPage ({ client }: Props) {
             <div className={CommonStyles.content}>
                 <h1 className={CommonStyles.title}>Administration</h1>
                 <div className={CommonStyles.itemsContainer}>
-                
-                    {
-                        client.auth.me?.isSuperAdmin && (
-                            <Link to="/admin/users" className={CommonStyles.button}>Utilisateurs</Link>
-                        )
-                    }
-                    {
-                        client.auth.me?.isAdmin && (
-                            <Link to="/admin/categories" className={CommonStyles.button}>Catégories</Link>
-                        )
-                    }
-                    {
-                        client.auth.me?.isModerator && (
-                            <Link to="/admin/validations" className={CommonStyles.button}>Validation de ressources</Link>
-                        )
-                    }
-                
+                    <div className={CommonStyles.buttonDiv}>
+                        {
+                            client.auth.me?.isSuperAdmin && (
+                                <Link to="/admin/users" className={CommonStyles.button}>Utilisateurs</Link>
+                            )
+                        }
+                        {
+                            client.auth.me?.isAdmin && (
+                                <Link to="/admin/categories" className={CommonStyles.button}>Catégories</Link>
+                            )
+                        }
+                        {
+                            client.auth.me?.isModerator && (
+                                <Link to="/admin/validations" className={CommonStyles.button}>Validation de ressources</Link>
+                            )
+                        }
+                    </div>
                 </div>
             </div>
         </div>
